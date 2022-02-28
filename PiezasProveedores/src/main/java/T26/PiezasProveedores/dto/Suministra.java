58512444c2e217c54a5401600066eb240f3ce878
JoanR17 <joanr.m.1999@gmail.com>
2022-02-28 09:52:26 +0100

    Ej 1

--- PiezasProveedores/src/main/java/T26/PiezasProveedores/dto/Suministra.java
+++ PiezasProveedores/src/main/java/T26/PiezasProveedores/dto/Suministra.java
@@ -11,7 +11,7 @@ import javax.persistence.*;
 @Table(name="suministra")
 public class Suministra {
 	
-	//Atributos proveedores
+	//Atributos suministra
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
