214e6c7d7002a345a1b329e5def98e5fc47218e7
JoanR17 <joanr.m.1999@gmail.com>
2022-02-28 08:55:13 +0100

    Ej 1

---
+++ PiezasProveedores/src/main/resources/data.sql
@@ -0,0 +1,27 @@
+DROP table IF EXISTS suministra;
+DROP table IF EXISTS proveedores;
+DROP table IF EXISTS piezas;
+
+create table piezas(
+	Id int auto_increment,
+	Nombre nvarchar(100),
+	PRIMARY KEY (id)
+);
+
+create TABLE proveedores(
+	Id char(4),
+	Nombre nvarchar(100),
+	PRIMARY KEY (id)
+);
+
+create table suministra(
+	Id int auto_increment,
+	CodigoPieza int,
+	IdProveedor char(4),
+	Precio int,
+	PRIMARY KEY (id),
+    KEY CodigoPieza_idx (CodigoPieza),
+    CONSTRAINT CodigoPieza_idx FOREIGN KEY (CodigoPieza) REFERENCES piezas (Id),
+    KEY IdProveedor_idx (IdProveedor),
+    CONSTRAINT IdProveedor_idx FOREIGN KEY (IdProveedor) REFERENCES proveedores (Id)
+);
